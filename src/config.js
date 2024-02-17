fd48e56fa51ffed964028fe39763f210a18cb83c
Y.Heckmann <thehecki97@gmail.com>
2024-02-17 21:34:31 +0100

    chore: added file with global config values

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const PRONOUNS_API = 'https://pronouns.alejo.io/api/';
+export const STREAMER_NAME_SELECTOR = '.channel-info-content a:not(.tw-halo)';
+export const FULLSCREEN_THEATER_STREAMER_SELECTOR = 'p[data-a-target="player-info-title"]';
